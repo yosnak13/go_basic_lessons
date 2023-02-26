b506c11e20d155fa0bf3ca65857a0abfb50e500c
yosnak13 <yosnak13@gmail.com>
2023-02-26 18:37:22 +0900

    string

--- archive/typing/main.go
+++ archive/typing/main.go
@@ -2,9 +2,28 @@ package main
 
 import "fmt"
 
-//boolean
+//string
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	var s string = "Hello Go"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	//複数行文字列はバッククォーテーションを使用
+	fmt.Println(`test
+	test
+		test`)
+	//文字列としてのダブルクォーテーションは２通り
+	fmt.Println("\"")
+	fmt.Println(`""`)
+
+	//文字列の1文字目を取得。
+	//byte配列になっていて、Hは72
+	//stringを指定して、その引数に取得したい対象を指定すると文字列になる。
+	fmt.Println(s[0])
+	fmt.Println(string(s[0]))
 }
